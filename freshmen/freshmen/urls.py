8009acab24240a00a6d10eafe7e7832df4902d92
limsomi <97082128+limsomi@users.noreply.github.com>
2023-02-18 22:34:23 +0900

    Delete freshmen directory

--- freshmen/freshmen/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('',RedirectView.as_view(url="/main/",permanent=True)),
-]
